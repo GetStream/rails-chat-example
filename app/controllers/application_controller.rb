7d79fe03d687352702bb6924f0064b362063a216
Mircea Cosbuc <mircea@getstream.io>
2019-05-22 16:24:33 +0200

    Add helper methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :user_token
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
+  def user_token
+    @user_token = session[:chat_token]
+  end
 end
